41020c7e0ee4a1f10d045ac01197fbdd81b3e11d
NorthCrypto1 <49313998+NorthCrypto1@users.noreply.github.com>
2019-10-28 21:58:31 -0500

    Settings Page Update
    This update includes update on:
    Settings page - buttons and Login
    Messenger - mute option(Not Working), adjusted friends messenger list, Adding new friend message chats currently not working but easy fix

--- src/messenger-page/messenger.js
+++ src/messenger-page/messenger.js
@@ -5,20 +5,19 @@ import MessengerNavigation from './messenger-navigation';
 import MessageSend from './messageSend';
 import './chatDisplay.css';
 import AddUser from './addUser';
+import Friends from './messenger.json';
 
 export default function Messenger(props) {
     const user = props.name;
     const chatText = Chat.chat
-    const friends = ['Michael', 'Jim', 'Pam', 'Dwight']
-
+    const friends = ['Michael', 'Jim', 'Pam', 'Dwight', 'Creed', 'Kevin', 'Angela', 'Ryan', 'Toby', 'Stanley', 'Kelly', 'Meredith']
     function onChangeAddName(newName) {
         friends.push(newName);
-        console.log(friends);
     }
 
     return (
         <div className="messenger">
-            <MessengerNavigation friendList={friends} />
+            <MessengerNavigation friendList={Friends} />
             <AddUser
                 addFriends={onChangeAddName}
             />
---
+++ src/wallet-page/wallet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Wallet Page
+//  Totalbalance 0000
+//      Address Lists withbalances
+//      List of Recent 10 transactions
+//   Chart of Spending Comparing Months
+//   Budget List
+//
+
+
+export default function Wallet() {
+    return (<div></div>)
+}
